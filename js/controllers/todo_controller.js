381317e8bcddc86c2a499cc22c6970dcb0ca712d
William Herry <WilliamHerryChina@Gmail.com>
2013-07-30 22:04:54 +0800

    adding child routes, template should exist before outlet

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -1,4 +1,21 @@
 Todos.TodoController = Ember.ObjectController.extend({
+  isEditing: false,
+
+  editTodo: function () {
+    this.set('isEditing', true);
+  },
+
+  removeTodo: function () {
+    var todo = this.get('model');
+    todo.deleteRecord();
+    todo.save();
+  },
+
+  acceptChanges: function () {
+    this.set('isEditing', false);
+    this.get('model').save();
+  },
+
   isCompleted: function(key, value) {
     var model = this.get('model');
 
@@ -11,23 +28,5 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-  }.property('model.isCompleted'),
-
-  isEditing: false,
-
-  editTodo: function() {
-    this.set('isEditing', true);
-  },
-
-  acceptChanges: function() {
-    console.log('acceptchanges');
-    this.set('isEditing', false);
-    this.get('model').save();
-  },
-
-  removeTodo: function() {
-    var todo = this.get('model');
-    todo.deleteRecord();
-    todo.save();
-  }
+  }.property('model.isCompleted')
 });
